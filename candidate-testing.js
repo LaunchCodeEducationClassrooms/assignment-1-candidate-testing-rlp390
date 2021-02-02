26209f4607e907018d7e074c51c8f32033d3dbed
rlp390 <rlp390@hotmail.com>
2021-02-02 20:59:58 +0000

    spaced

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,7 +5,7 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space?  ";
+let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer;
 let questions = ["Who was the first American woman in space?", "True or false:  5000 meters = 5 kilometers?", "(5 + 3) / 2 * 10 = ?", "Given the array [8, Orbit, Trajectory, 45], what entry is at index 2?", "What is the minimum crew size for the International Space Station (ISS)?"];
@@ -48,7 +48,7 @@ function gradeQuiz(candidateAnswers) {
   }
   console.log(`>>> Overall Grade:  ${grade}% (${gradeCorrect} of 5 responses correct) <<<`);
   console.log(`>>> Status: ${status} <<<`);
-  
+
   return grade;
 }
 
